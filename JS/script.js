58178f781b15e70d9b963d93afdfd3c3be6bc9cf
sam-aggrey <78800769+sam-aggrey@users.noreply.github.com>
2021-02-15 18:07:24 +0000

    Update script.js
    Deleted my favorite food  variable, removed the hello world alert, and created an array with several objects. Each object has a name,height, and type.

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,60 @@
-alert("Hello World!");
-let favoriteFood = 'Banku';
-document.write(favoriteFood);
+let pokemonList=[
+    
+    {
+        name: 'Charmander',
+        height:0.6,
+        type:["Monster", "Dragon"]
+    },
+    
+    {
+       name: 'Bulbasaur',
+        height:0.7,
+        type:["Monster", "Grass"]
+    },
+    
+    {
+        name: 'Blastoise',
+        height:0.6,
+        type:["Monster", "Water 1"]
+    },
+    
+    {
+        name: 'Ivysaur',
+        height:1,
+        type:["Monster", "Grass"]
+    },
+    
+    {
+        name: 'Venusaur',
+        height:2,
+        type:["Monster", "Grass"]
+    },
+    
+    {
+        name: 'Charizard',
+        height:1.7,
+        type:["Monster", "Dragon"]
+    },
+    
+     {
+        name: 'Nidoking',
+        height:1.4,
+        type:["Monster", "Field"]
+    },
+    
+     {
+        name: 'Nidorino',
+        height:0.9,
+        type:["Monster", "Field"]
+    },
+    
+     {
+        name: 'Charmeleon',
+        height:1.1,
+        type:["Monster", "Dragon"]
+    },
+    
+    
+    
+];
+
